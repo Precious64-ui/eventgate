bcb53456b94f69d1c14a96946a6773ed85992c53
Precious64-ui <iwuchukwuprecious64@gmail.com>
2026-08-16 13:45:23 +0100

    Fix production login API

--- client/login.js
+++ client/login.js
@@ -91,21 +91,21 @@ form.addEventListener("submit", async (e) => {
 
     try {
 
-        const response = await fetch(
-            "http://localhost:5000/api/users/login",
-            {
-                method: "POST",
+       const response = await fetch(
+    "https://eventgate-fxp8.onrender.com/api/users/login",
+    {
+        method: "POST",
 
-                headers: {
-                    "Content-Type": "application/json"
-                },
+        headers: {
+            "Content-Type": "application/json"
+        },
 
-                body: JSON.stringify({
-                    email,
-                    password
-                })
-            }
-        );
+        body: JSON.stringify({
+            email,
+            password
+        })
+    }
+);
 
 
         const data = await response.json();
